cfeebba368f70c5c44f1ff173e07089079aa0a57
오유진 <omh232323@gmail.com>
2022-12-29 13:21:57 +0900

    Before(query)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
+import Chart from "./screens/Chart";
 import Coin from "./screens/Coin";
 import Coins from "./screens/Coins";
+import Price from "./screens/Price";
 
 const router = createBrowserRouter([
   {
@@ -13,11 +15,45 @@ const router = createBrowserRouter([
         element: <Coins />,
       },
       {
-        path: "/:coinId",
+        path: ":coinId",
         element: <Coin />,
+        children: [
+          {
+            path: "chart",
+            element: <Chart />,
+          },
+          {
+            path: "price",
+            element: <Price />,
+          },
+        ],
       },
     ],
   },
 ]);
+
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/",
+//       element: <Root />,
+//     },
+//     {
+//       path: "/:coinID",
+//       element: <Coins />,
+//       children: [
+//         {
+//           path: "chart",
+//           element: <Chart />,
+//         },
+//         {
+//           path: "price",
+//           element: <Price />,
+//         },
+//       ],
+//     },
+//   ]
+//   { basename: "/reactMasterCrypto" }
+// );
 
 export default router;
--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -1,7 +1,7 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation, useMatch, useParams } from "react-router-dom";
 import styled from "styled-components";
+import StyledLink from "../components/StyledLink";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,6 +15,7 @@ const Header = styled.header`
   justify-content: center;
   align-items: center;
   font-size: 50px;
+  margin-bottom: 30px;
 `;
 
 const Title = styled.h1`
@@ -38,20 +39,127 @@ const Loading = styled.div`
   text-align: center;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+  margin-bottom: 25px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+const Tab = styled.div<{ isActive: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+  margin-bottom: 25px;
+  a {
+    color: ${(props) =>
+      props.isActive ? props.theme.accentColor : props.theme.textColor};
+  }
+`;
+
+interface IinfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  // tags: object;
+  // team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  // links: object;
+  // links_extended: object;
+  // whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IpriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_15m: number;
+      percent_change_30m: number;
+      percent_change_1h: number;
+      percent_change_6h: number;
+      percent_change_12h: number;
+      percent_change_24h: number;
+      percent_change_7d: number;
+      percent_change_30d: number;
+      percent_change_1y: number;
+      ath_price: number;
+      ath_date: string;
+      percent_from_price_ath: number;
+    };
+  };
+}
+
 function Coin() {
   const { coinId } = useParams();
   const [loading, setLoading] = useState(true);
   const loc = useLocation();
-  const name: string = loc.state;
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<IinfoData>();
+  const [priceInfo, setPriceInfo] = useState<IpriceData>();
+  const priceMatch = useMatch(`/${coinId}/price`);
+  const chartMatch = useMatch(`/${coinId}/chart`);
+
   const getDetail = async () => {
-    const infoData = await axios(`coinpaprika/v1/coins/${coinId}`);
-    console.log(infoData);
-    const priceData = await axios(`coinpaprika/v1/tickers/${coinId}`);
-    console.log(priceData);
+    const infoData = await (
+      await fetch(`coinpaprika/v1/coins/${coinId}`)
+    ).json();
+    const priceData = await (
+      await fetch(`coinpaprika/v1/tickers/${coinId}`)
+    ).json();
     setInfo(infoData);
-    setPriceInfo(priceInfo);
+    setPriceInfo(priceData);
     setLoading(false);
   };
   useEffect(() => {
@@ -60,9 +168,55 @@ function Coin() {
   return (
     <Container>
       <Header>
-        <Title>{name || "hello world"}</Title>
+        <Title>
+          {loc.state?.name
+            ? loc.state.name
+            : loading
+            ? "Loading..."
+            : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loading>Loading...</Loading> : <h1>hi</h1>}
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={priceMatch !== null}>
+              <StyledLink to={`price`}>price</StyledLink>
+            </Tab>
+            <Tab isActive={chartMatch !== null}>
+              <StyledLink to={`chart`}>chart</StyledLink>
+            </Tab>
+          </Tabs>
+
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
